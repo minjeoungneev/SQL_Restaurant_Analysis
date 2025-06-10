4ede2ae1d0dc4b16d1c474ab833ca81c66d04de5
Minjeoung (MJ) <139029169+minjeoungneev@users.noreply.github.com>
2025-06-10 15:08:17 -0400

    Update project.sql

--- project.sql
+++ project.sql
@@ -60,11 +60,50 @@ select order_id, count(item_id) as num_of_items
 from order_details
 group by order_id
 order by count(item_id) desc
-limit 1
+
 
 --8.How many orders had more than 12 items?
-with tbl1 as (select order_id, count(item_id) as num_of_items
+select count(*) from 
+(select order_id, count(item_id) as num_of_items
 from order_details
 group by order_id
-having count(item_id) > 12)
-select count(order_id) from tbl1
+having num_of_items >12) as num_orders
+
+--9.Combine the menu_items and order_details tables into a single table
+select a.*, b.item_name, b.category, b.price
+from order_details a left join menu_items b on a.item_id = b.menu_item_id
+
+
+--10.What were the least and most ordered items? What categories were they in?
+(select category, item_name, count(order_details_id) as num_ordered
+from menu_items a left join order_details b on a.menu_item_id = b.item_id
+group by category, item_name
+order by num_ordered desc
+limit 1)
+union
+
+(select category, item_name, count(order_details_id) as num_ordered
+from menu_items a left join order_details b on a.menu_item_id = b.item_id
+group by category, item_name
+order by num_ordered
+limit 1)
+
+--11.What were the top 5 orders that spent the most money?
+select order_id, sum(price) as total_spend
+from order_details a left join menu_items b on a.item_id = b.menu_item_id
+group by order_id
+order by sum(price) desc
+limit 5
+
+--12.View the details of the highest spend order. Which specific items were purchased?
+select category, item_name, count(item_id) as num_of_items
+from order_details a left join menu_items b on a.item_id = b.menu_item_id
+where order_id = 440
+group by category, item_name
+
+--13.View the details of the top 5 highest spend orders
+select order_id, category, count(item_id) as num_of_items
+from order_details a left join menu_items b on a.item_id = b.menu_item_id
+where order_id in (440, 2075, 1957, 330, 2675)
+group by order_id, category
+
